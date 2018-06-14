e89197e2f2dc2124ec8cf5a1964f15a6ec887355
luwata <yacine.souadda@gmail.com>
2018-06-14 17:36:32 +0200

    modif sur l'exercice

--- api.py
+++ api.py
@@ -16,6 +16,7 @@ def pokemonDisplayAll():
     db.close()
     return f
 
+
 @hug.post('/pokemon/add')
 def pokemonAdd(ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed):
     """ Ajoute un pokemon """
@@ -28,14 +29,26 @@ def pokemonAdd(ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, s
     db.close()
     return f
 
-@hug.delete('/pokemon/add')
-def pokemonAdd(ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed):
-    """ Ajoute un pokemon """
+
+@hug.put('/pokemon/update')
+def pokemonUpdate(pokname, ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed):
+    """ Modifie un pokemon """
     db = mysql.connector.connect(host="localhost", user="root", password="", database="pokebase")
     cursor = db.cursor()
-    req = (ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed)
-    f = cursor.execute("""INSERT INTO pokemon (ref_pokemon, nom, type, total, hp, attack, defense, sp_atk, sp_def, speed) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", req)
+    req = (ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed, pokname)
+    f = cursor.execute("""UPDATE pokemon SET ref_pokemon=%s, nom=%s, type=%s, total=%s, hp=%s, attack=%s, defense=%s, sp_atk=%s, sp_def=%s, speed=%s WHERE nom =%s""", req)
     db.commit()
     cursor.close()
     db.close()
     return f
+
+
+@hug.delete('/pokemon/delete')
+def pokemonDel(namepok):
+    """ Supprime un pokemon """
+    db = mysql.connector.connect(host="localhost", user="root", password="", database="pokebase")
+    cursor = db.cursor()
+    cursor.execute("""DELETE FROM pokemon WHERE nom =(?)""", namepok)
+    db.commit()
+    cursor.close()
+    db.close()
