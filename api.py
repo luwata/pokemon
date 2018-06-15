b888aa8a09c8ce89f0e28b831e09349821a78b3e
luwata <yacine.souadda@gmail.com>
2018-06-15 12:14:36 +0200

    Ajout des verification sur requests api

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 # filename: api.py
-"""A basic (single function) API written using hug"""
+"""API Pokemons"""
 
 import hug
 import mysql.connector
@@ -22,12 +22,21 @@ def pokemonAdd(ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, s
     """ Ajoute un pokemon """
     db = mysql.connector.connect(host="localhost", user="root", password="", database="pokebase")
     cursor = db.cursor()
-    req = (ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed)
-    f = cursor.execute("""INSERT INTO pokemon (ref_pokemon, nom, type, total, hp, attack, defense, sp_atk, sp_def, speed) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", req)
-    db.commit()
+
+    cursor.execute("""SELECT EXISTS(SELECT * FROM pokemon WHERE nom =%s )""", (nom, ))
+    test = cursor.fetchone()
+    if int(test[0]) == 0:
+        req = (ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed)
+        cursor.execute("""INSERT INTO pokemon (ref_pokemon, nom, type, total, hp, attack, defense, sp_atk, sp_def, speed) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", req)
+        db.commit()
+        cursor.execute("""SELECT * FROM pokemon WHERE nom=%s""", (nom,))
+        f = cursor.fetchone()
+        result = 'Votre pokemon {0} a été ajouté'.format(str(f[0]))
+    else:
+        result = 'Votre pokemon existe deja !'
     cursor.close()
     db.close()
-    return f
+    return result
 
 
 @hug.put('/pokemon/update')
@@ -35,20 +44,34 @@ def pokemonUpdate(pokname, ref_pokemon, nom, type_pok, total, hp, attack, defens
     """ Modifie un pokemon """
     db = mysql.connector.connect(host="localhost", user="root", password="", database="pokebase")
     cursor = db.cursor()
-    req = (ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed, pokname)
-    f = cursor.execute("""UPDATE pokemon SET ref_pokemon=%s, nom=%s, type=%s, total=%s, hp=%s, attack=%s, defense=%s, sp_atk=%s, sp_def=%s, speed=%s WHERE nom =%s""", req)
-    db.commit()
+    cursor.execute("""SELECT EXISTS(SELECT * FROM pokemon WHERE nom =%s )""", (nom,))
+    test = cursor.fetchone()
+    if int(test[0]) == 0:
+        req = (ref_pokemon, nom, type_pok, total, hp, attack, defense, sp_atk, sp_def, speed, pokname)
+        cursor.execute("""UPDATE pokemon SET ref_pokemon=%s, nom=%s, type=%s, total=%s, hp=%s, attack=%s, defense=%s, sp_atk=%s, sp_def=%s, speed=%s WHERE nom =%s""", req)
+        db.commit()
+        cursor.execute("""SELECT * FROM pokemon WHERE nom=%s""", (nom, ))
+        f = cursor.fetchone()
+        result = 'Votre pokemon {0} a été mis à jour en {1}'.format(pokname, f[2])
+    else:
+        result = 'Votre pokemon existe deja !'
     cursor.close()
     db.close()
-    return f
-
+    return result
 
 @hug.delete('/pokemon/delete')
 def pokemonDel(namepok):
     """ Supprime un pokemon """
     db = mysql.connector.connect(host="localhost", user="root", password="", database="pokebase")
     cursor = db.cursor()
-    cursor.execute("""DELETE FROM pokemon WHERE nom =(?)""", namepok)
-    db.commit()
+    cursor.execute("""SELECT EXISTS(SELECT * FROM pokemon WHERE nom =%s )""", (namepok,))
+    test = cursor.fetchone()
+    if int(test[0]) == 0:
+        result = 'Le pokemon que vous essayez de supprimer n existe pas !'
+    else:
+        cursor.execute("""DELETE FROM pokemon WHERE nom =%s""", (namepok, ))
+        db.commit()
+        result = 'Votre pokemon a bien été supprimé !'
     cursor.close()
     db.close()
+    return result
